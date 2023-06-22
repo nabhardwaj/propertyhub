5b4dd35c15837b55903f1acd85ea2fdb3f8d0804
nabhardwaj <bhardwaj.ashwani92@gmail.com>
2023-06-22 23:21:32 +0530

    update vite

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/propertyhub/',
   plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    vue()
+  ]
 })
